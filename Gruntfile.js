0c6f77b9f0ccbb7c0826fa7785abdb134f186fee
bom2ne1 <jgcaley@gmail.com>
2025-01-22 16:56:29 +1100

    added grunt and less

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      development: {
+        files: {
+          "dist/css/styles.css": "src/less/styles.less"
+        }
+      }
+    },
+
+    copy: {
+      html: {
+        expand: true,
+        cwd: 'src/',
+        src: 'index.html',
+        dest: 'dist/',
+      },
+      assets: {
+        expand: true,
+        cwd: 'src/assets/',
+        src: '**',
+        dest: 'dist/assets/',
+      }
+    },
+
+    watch: {
+      styles: {
+        files: ['src/less/**/*.less'],
+        tasks: ['less', 'copy'],
+        options: {
+          spawn: false
+        }
+      },
+      html: {
+        files: ['src/index.html'],
+        tasks: ['copy'],
+        options: {
+          spawn: false
+        }
+      },
+      assets: {
+        files: ['src/assets/**/*'],
+        tasks: ['copy'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'dist/index.html',  
+            'dist/css/*.css',    
+            'dist/js/*.js',      
+            'dist/assets/**/*'   
+          ]
+        },
+        options: {
+          watchTask: true,  
+          server: {
+            baseDir: './dist' 
+          }
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');  
+
+  grunt.registerTask('default', ['less', 'copy', 'browserSync', 'watch']);
+};
