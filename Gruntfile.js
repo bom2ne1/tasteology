6136dc0118362472cf9436982ed0de78c05968bb
bom2ne1 <jgcaley@gmail.com>
2025-01-23 12:32:47 +1100

    Update gruntfile to correctly copy js to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,91 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    less: {
-      development: {
-        files: {
-          "dist/css/styles.css": "src/less/styles.less"
-        }
-      }
-    },
-
-    copy: {
-      html: {
-        expand: true,
-        cwd: 'src/',
-        src: 'index.html',
-        dest: 'dist/',
-      },
-      assets: {
-        expand: true,
-        cwd: 'src/assets/',
-        src: '**',
-        dest: 'dist/assets/',
-      }
-    },
-
-    watch: {
-      styles: {
-        files: ['src/less/**/*.less'],
-        tasks: ['less', 'copy'],
-        options: {
-          spawn: false
-        }
-      },
-      html: {
-        files: ['src/index.html'],
-        tasks: ['copy'],
-        options: {
-          spawn: false
-        }
-      },
-      assets: {
-        files: ['src/assets/**/*'],
-        tasks: ['copy'],
-        options: {
-          spawn: false
-        }
-      }
-    },
-
-    browserSync: {
-      dev: {
-        bsFiles: {
-          src: [
-            'dist/index.html',  
-            'dist/css/*.css',    
-            'dist/js/*.js',      
-            'dist/assets/**/*'   
-          ]
+        less: {
+            development: {
+                files: {
+                    "dist/css/styles.css": "src/less/styles.less"
+                }
+            }
         },
-        options: {
-          watchTask: true,  
-          server: {
-            baseDir: './dist' 
-          }
+
+        copy: {
+            html: {
+                expand: true,
+                cwd: 'src/',
+                src: 'index.html',
+                dest: 'dist/',
+            },
+            js: {
+                expand: true,
+                cwd: 'src/js/',
+                src: '**/*.js',  // Add this to copy all JS files
+                dest: 'dist/js/',
+            },
+            assets: {
+                expand: true,
+                cwd: 'src/assets/',
+                src: '**',
+                dest: 'dist/assets/',
+            }
+        },
+
+        watch: {
+            styles: {
+                files: ['src/less/*.less'],
+                tasks: ['less', 'copy'],
+                options: {
+                    spawn: false
+                }
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['copy'],
+                options: {
+                    spawn: false
+                }
+            },
+            js: {
+                files: ['src/js/*.js'],
+                tasks: ['copy'],
+                options: {
+                    spawn: false
+                }
+            },
+            assets: {
+                files: ['src/assets/**/*'],
+                tasks: ['copy'],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'dist/index.html',
+                        'dist/css/*.css',
+                        'dist/js/*.js',
+                        'dist/assets/**/*'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './dist'
+                    }
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browser-sync');  
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
 
-  grunt.registerTask('default', ['less', 'copy', 'browserSync', 'watch']);
+    grunt.registerTask('default', ['less', 'copy', 'browserSync', 'watch']);
 };
